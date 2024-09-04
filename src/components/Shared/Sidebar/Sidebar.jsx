76021d214e191860ed2a07bd767bfa50ee0e1524
shihab01118 <shahed.shihab.0@gmail.com>
2024-09-04 17:59:53 +0600

    sidebar modified

--- src/components/Shared/Sidebar/Sidebar.jsx
+++ src/components/Shared/Sidebar/Sidebar.jsx
@@ -1,6 +1,10 @@
 import { NavLink } from "react-router-dom";
 import useCurrentUser from "../../../hooks/useCurrentUser";
 
+// icons
+import { BiSolidDashboard } from "react-icons/bi";
+// import { BsCalendar2Plus, BsCalendarWeekFill } from "react-icons/bs";
+
 // styles
 import "./Sidebar.css";
 
@@ -10,28 +14,55 @@ const Sidebar = () => {
   //  navlinks for patient
   const patientNavlinks = (
     <>
-      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark">
-        <NavLink to="/dashboard/patient">
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink to="/dashboard/patient" className="flex items-center gap-2.5">
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
           <span>Dashboard</span>
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/patient/book-appointment">
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/patient/book-appointment"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
           <span>Book Appointment</span>
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/patient/my-appointments">
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/patient/my-appointments"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
           <span>My Appointments</span>
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/patient/my-prescriptions">
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/patient/my-prescriptions"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
           <span>My Prescriptions</span>
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/patient/profile">
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/patient/profile"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
           <span>Profile</span>
         </NavLink>
       </li>
@@ -40,42 +71,114 @@ const Sidebar = () => {
   // navlinks for doctor
   const doctorNavlinks = (
     <>
-      <li>
-        <NavLink to="/dashboard/doctor">Dashboard</NavLink>
-      </li>
-      <li>
-        <NavLink to="/dashboard/doctor/my-appointments">
-          My Appointments
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink to="/dashboard/doctor" className="flex items-center gap-2.5">
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
+          <span>Dashboard</span>
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/doctor/patients">Patients</NavLink>
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/doctor/my-appointments"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
+          <span>My Appointments</span>
+        </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/doctor/profile">Profile</NavLink>
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/doctor/patients"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
+          <span>Patients</span>
+        </NavLink>
+      </li>
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/doctor/profile"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
+          <span>Profile</span>
+        </NavLink>
       </li>
     </>
   );
   // navlinks for admin
   const adminNavlinks = (
     <>
-      <li>
-        <NavLink to="/dashboard/admin">Dashboard</NavLink>
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink to="/dashboard/admin" className="flex items-center gap-2.5">
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
+          <span>Dashboard</span>
+        </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/admin/appointments">Appointments</NavLink>
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/admin/appointments"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
+          <span>Appointments</span>
+        </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/admin/doctors">Doctors</NavLink>
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/admin/doctors"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
+          <span>Doctors</span>
+        </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/admin/patients">Patients</NavLink>
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/admin/patients"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
+          <span>Patients</span>
+        </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/admin/reports">Reports</NavLink>
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/admin/reports"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
+          <span>Reports</span>
+        </NavLink>
       </li>
-      <li>
-        <NavLink to="/dashboard/admin/settings">Settings</NavLink>
+      <li className="py-3 text-dashboard-light text-[15px] font-medium hover:text-dashboard-dark transition duration-200">
+        <NavLink
+          to="/dashboard/admin/settings"
+          className="flex items-center gap-2.5"
+        >
+          <div>
+            <BiSolidDashboard className="text-xl" />
+          </div>{" "}
+          <span>Settings</span>
+        </NavLink>
       </li>
     </>
   );
@@ -89,7 +192,7 @@ const Sidebar = () => {
 
   return (
     <nav className="w-[250px] bg-white h-[calc(100vh-88px)] shadow-sm rounded-tr-[40px] px-5 pt-2.5">
-      <ul className="flex flex-col">{navlinks}</ul>
+      <ul className="flex flex-col navlinks">{navlinks}</ul>
     </nav>
   );
 };
